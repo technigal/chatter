fa37922391e9cad76ec1d144ee40b563d8997acf
Ashley Boldin <ashleyboldin@me.com>
2015-06-15 20:48:57 -0500

    Adding hiccup and html page code

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler chatter.handler/app}
   :profiles

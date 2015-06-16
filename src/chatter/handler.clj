5c378850235b7b2e4562b29029cb7e5107a56cbb
Ashley Boldin <ashleyboldin@me.com>
2015-06-15 21:37:10 -0500

    Adding functionality to add and display new messages

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,20 +2,45 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as page]))
+            [ring.middleware.params :refer [wrap-params]]
+            [hiccup.page :as page]
+            [hiccup.form :as form]))
+
+(def chat-messages
+    (atom '()))
+
 
 (defn generate-message-view
   "This generates the HTML for displaying messages"
-   []
+   [messages]
     (page/html5
         [:head
          [:title "chatter"]]
          [:body
-          [:h1 "Our Chat App"]]))
+          [:h1 "Our Chat App"]
+          [:p
+           (form/form-to
+            [:post "/"]
+            "Name: " (form/text-field "name")
+            "Message: " (form/text-field "msg")
+            (form/submit-button "Submit"))]
+          [:p
+            [:table
+            (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
+
+(defn update-messages!
+  "This will update a message list atom"
+  [messages name new-message]
+  (swap! messages conj {:name name :message new-message}))
 
 (defroutes app-routes
-  (GET "/" [] (generate-message-view))
+  (GET "/" [] (generate-message-view @chat-messages))
+  (POST "/" {params :params}
+      (let [name-param (get params "name")
+            msg-param (get params "msg")
+            new-messages (update-messages! chat-messages name-param msg-param)]
+        (generate-message-view new-messages)
+            ))
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-params app-routes))
